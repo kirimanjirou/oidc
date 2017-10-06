175636eb6c597145dc73a79bdaa3f64c6b7b6578
root <root@dti-vps-srv728.(none)>
2017-10-06 17:50:09 +0900

    Initail Commit

---
+++ routes/google_callback.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var router = express.Router();
+var SampleWebFunction = require('../SampleWebFunction.js');
+var RestClient = require('node-rest-client').Client;
+
+
+/* GET home page. */
+router.get('/',function(req, res, next) {
+
+	var state = req.query.state;
+	var code = req.query.code;
+	var session_state = req.session.state;
+
+	console.log("STATE:" + state);
+	console.log("CODE:"  + code);
+	console.log("SESSION_STATE:" + session_state);
+	
+	if ( state != session_state){
+		console.log("state parameter is unvalid");
+		var state = SampleWebFunction.getUniqueStr();
+		req.session.state = state;
+		res.redirect('/');
+	}else{
+
+		var accessTokenClient = new RestClient();
+	
+		var accessTokenClientArgs = {
+			headers : {
+	        		"Content-Type" : "application/x-www-form-urlencoded",
+	                },
+	                data: {
+	                        grant_type : "authorization_code",
+	                        code : code,
+	                        redirect_uri : ENTER_YOUR_REDIRECTURI_FOR_GOOGLE,
+				client_id : ENTERYOURCLIRNTID_FOR_GOOGLE,
+				client_secret : ENTERYOURCLIENTSECRET_FOR_GOOGLE
+	                }
+		};
+			
+		accessTokenClient.post("https://www.googleapis.com/oauth2/v4/token", accessTokenClientArgs, function (data, response) {
+			if( data.error ){
+	        	        console.log("accessTokenClient is failed: ");
+				console.log(data);
+				res.redirect('/');
+			}else{
+				var bearer =  "Bearer " + data.access_token;
+				console.log("ACCESS_TOKEN:" + data.access_token);
+				SampleWebFunction.verifyIdToken(data.id_token,  function (err, data){
+					if(err){
+						console.log("ID Token Verify is failed: ");
+						console.log(data);
+						res.redirect('/');
+					}else{
+						var userInfoClientArgs = {
+							headers : {
+								'Authorization' : bearer
+							}
+						};
+						console.log(userInfoClientArgs);
+						var userInfoClient = new RestClient();
+						userInfoClient.get("https://www.googleapis.com/oauth2/v3/userinfo", userInfoClientArgs, function (data, response) {
+							if( data.error ){
+								console.log("userInfoClient is failed: ");
+								console.log(data);
+								res.redirect('/');
+							}else{
+								console.log(data);
+								res.render('users', {
+									displayName: data.name,
+									pictureUrl: data.picture,
+									statusMessage: "",
+									openId : data.sub,
+									state: state
+								});
+							}
+						});
+					}
+				});
+			}	
+		});	
+	}
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+var SampleWebFunction = require('../SampleWebFunction.js');
+var RestClient = require('node-rest-client').Client;
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+	var state = SampleWebFunction.getUniqueStr();
+	req.session.state = state;
+	res.render('index', { state: state });
+});
+
+router.get('/callback',function(req, res, next) {
+
+	var state = req.query.state;
+	var code = req.query.code;
+	var session_state = req.session.state;
+	/*
+	console.log("STATE:" + state);
+	console.log("CODE:"  + code);
+	console.log("SESSION_STATE:" + session_state);
+	*/
+	if ( state != session_state){
+		console.log("state parameter is unvalid");
+		var state = SampleWebFunction.getUniqueStr();
+		req.session.state = state;
+		res.redirect('/');
+	}
+
+	var accessTokenClient = new RestClient();
+
+	var accessTokenClientArgs = {
+		headers : {
+        		"Content-Type" : "application/x-www-form-urlencoded"
+                },
+                data: {
+                        grant_type : "authorization_code",
+                        code : code,
+                        client_id : ENTER_YOUR_CLIENTID_FOR_LINE
+                        client_secrea : ENTER,
+                        redirect_uri : ENTER
+                }
+	};
+		
+	accessTokenClient.post("https://api.line.me/v2/oauth/accessToken", accessTokenClientArgs, function (data, response) {
+		if( data.error ){
+        	        console.log("accessTokenClient is failed: ");
+			console.log(data);
+			res.redirect('/');
+		}
+		var bearer =  "'Bearer " + data.access_token + "'";
+
+		//console.log("ACCESS_TOKEN:" + data.access_token);
+		var userInfoClientArgs = {
+                        headers : {
+                                'Authorization' : bearer
+                        }
+		};
+		var userInfoClient = new RestClient();
+		userInfoClient.get("https://api.line.me/v2/profile", userInfoClientArgs, function (data, response) {
+			if( data.error ){
+				console.log("userInfoClient is failed: ");
+				console.log(data);
+				res.redirect('/');
+			}
+			console.log(data);
+			res.render('users', {
+				displayName: data.displayName,
+				pictureUrl: data.pictureUrl,
+				statusMessage: data.statusMessage,
+				openId : data.userId,
+				state: state
+			});	
+		});
+	});	
+});
+
+module.exports = router;
